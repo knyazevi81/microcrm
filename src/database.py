ce2986faf107f8b8bef68430cb95d1263e8d4d69
Хасан Хамраев <khasanknyazev81@gmail.com>
2025-06-23 00:21:06 +0300

    removed garbage modules

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column, Mapped
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 from src.config import settings
 
