95918e6e62c602b47fce30326c53d4edc976972b
Хасан Хамраев <khasanknyazev81@gmail.com>
2025-06-23 16:43:16 +0300

    Update config.py: add root creds

--- src/config.py
+++ src/config.py
@@ -7,6 +7,13 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
 
+    SECRET_KEY: str
+    ALG: str
+
+    ROOT_EMAIL: str
+    ROOT_USERNAME: str
+    ROOT_PASSWORD: str
+
     @property
     def DATABASE_URL(self,) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
