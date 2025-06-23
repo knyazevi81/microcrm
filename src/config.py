7eb39eba45e0896a4410f7d358086cc10a111ce7
Хасан Хамраев <khasanknyazev81@gmail.com>
2025-06-23 21:40:19 +0300

    Update config.py: add redis env creds

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
@@ -14,13 +15,28 @@ class Settings(BaseSettings):
     ROOT_USERNAME: str
     ROOT_PASSWORD: str
 
+    REDIS_HOST: str = "redis"
+    REDIS_PORT: int = 6379
+    REDIS_PASSWORD: str = ""
+    REDIS_USER: str
+    REDIS_USER_PASSWORD: str
+
     @property
-    def DATABASE_URL(self,) -> str:
+    def redis_url(self):
+        if self.REDIS_PASSWORD:
+            return f"redis://{self.REDIS_USER}:{self.REDIS_USER_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+        else:
+            return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+
+    @property
+    def DATABASE_URL(
+        self,
+    ) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     class Config:
         env_file = ".env"
-        extra="allow"
-
+        extra = "allow"
 
 
 settings = Settings()  # type: ignore
